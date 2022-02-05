9266e41c27af43dbcb55586358b27f1b153f4d69
sinmugyeol <tlsanruf123@gmail.com>
2022-02-06 00:58:40 +0900

    [add] Focus layer

--- models/layers.py
+++ models/layers.py
@@ -14,14 +14,47 @@ class CBR2d(nn.Module):
 
 # short-cut Connection
 class Bottleneck(nn.Module):
-    def __init__(self, in_channels, out_channels, shortcut=True, g=1):
+    def __init__(self, in_channels, out_channels, shortcut=True, g=1, e=0.5):
+        """
+        :param in_channels: input channels of number
+        :param out_channels: output channels of number
+        :param shortcut: connection x
+        :param g: groups
+        :param e: expansion
+        """
         super(Bottleneck, self).__init__()
-        self.conv1 = CBR2d(in_channels, in_channels // 2, stride=1, kernel=1)
-        self.conv2 = CBR2d(in_channels // 2, out_channels, stride=1, kernel=3, padding=1, group=g)
+        channels = (out_channels * e)
+        self.conv1 = CBR2d(in_channels, channels, stride=1, kernel=1)
+        self.conv2 = CBR2d(channels, out_channels, stride=1, kernel=3, padding=1, group=g)
         self.add = shortcut and in_channels == out_channels
 
     def forward(self, x):
         return x + self.conv2(self.conv1(x)) if self.add else self.conv2(self.conv1(x))
+
+class BottleneckCSP(nn.Module):
+    def __init__(self, in_channels, out_channels, num=1, shortcut=True, g=1, e=0.5):
+        """
+        :param in_channels: input channels of number
+        :param out_channels: output channels of number
+        :param num: number of repeat
+        :param shortcut: using shortcut connection
+        :param g: groups
+        :param e: expansion
+        """
+        super(BottleneckCSP, self).__init__()
+        channels = (out_channels * e)
+        self.conv1 = CBR2d(in_channels, channels, kernel=1, stride=1)
+        self.conv2 = nn.Conv2d(in_channels, channels, kernel_size=1, stride=1, bias=False)
+        self.conv3 = nn.Conv2d(channels, channels, kernel_size=1, stride=1, bias=False)
+        self.conv4 = CBR2d(in_channels, out_channels, kernel=1, stride=1)
+        self.bn = nn.BatchNorm2d(in_channels)
+        self.lrelu = nn.LeakyReLU(0.1, inplace=True)
+        self.m = nn.Sequential(*[Bottleneck(channels, channels, shortcut, g, e=1.0) for i in range(num)])
+
+    def forward(self, x):
+        x1 = self.conv3(self.m(self.conv1(x)))
+        x2 = self.conv2(x)
+        return self.conv4(self.lrelu(self.bn(torch.cat((x1, x2), dim=1))))
 
 # Spatial Pyramid Pooling
 class SPP(nn.Module):
@@ -36,3 +69,24 @@ class SPP(nn.Module):
         x = self.cv2(torch.cat([x] + [maxpool(x) for maxpool in self.max], 1))
 
         return x
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+class Focus(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel=1, stride=1, padd=None, g=1, relu=True):
+        super(Focus, self).__init__()
+        self.conv = CBR2d(in_channels * 4, out_channels, kernel=kernel, stride=stride, padding=padd, group=g, relu=relu)
+
+    def forward(self, x): # x(batch, channels, width, height) -> y(batch, 4*channels, width / 2, height / 2)
+        return self.conv(torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1))
+
+class Concat(nn.Module):
+    def __init__(self, dimension=1):
+        super(Concat, self).__init__()
+        self.d = dimension
+
+    def forward(self, x):
+        return torch.cat(x, self.d)
